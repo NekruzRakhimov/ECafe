5b3049304f0b33b07e4c685bf652b11bc585a91a
Александр <157706042+MAF4ik@users.noreply.github.com>
2024-02-01 23:25:19 +0300

    [features]

--- repository.py
+++ repository.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 def get_menu():
     with Session(autoflush=False, bind=engine) as db:
-        return db.query(Menu).filter(is_deleted == False).all()
+        return db.query(Menu).filter(Menu.is_deleted == False).all()
 
 
 def create_dish(dish):
@@ -19,11 +19,33 @@ def create_dish(dish):
 
 def delete_dish(_id):
     with Session(autoflush=False, bind=engine) as db:
-        dish = db.query(Menu).filter(and_(id == _id, is_deleted == False)).first()
+        dish = db.query(Menu).filter(and_(Menu.id == _id, Menu.is_deleted == False)).first()
         if dish:
             dish.is_deleted = True
             db.add(dish)
             db.commit()
         else:
-            raise ValueError("Personal data not found")
+            raise ValueError("Menu data not found")
 
+
+def price_change(_id, new_price):
+    with Session(autoflush=False, bind=engine) as db:
+        dish = db.query(Menu).filter(and_(Menu.id == _id, Menu.is_deleted == False)).first()
+        if dish:
+            dish.price = new_price
+            db.add(dish)
+            db.commit()
+        else:
+            raise ValueError("Menu data not found")
+
+
+def status_change(_id):
+    with Session(autoflush=False, bind=engine) as db:
+        dish = db.query(Menu).filter(and_(Menu.id == _id, Menu.is_deleted == False)).first()
+        if dish:
+            dish.status = "Готово"
+            dish.status_time = 0
+            db.add(dish)
+            db.commit()
+        else:
+            raise ValueError("Menu data not found")
--- routes.py
+++ routes.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from flask import jsonify, Blueprint, request
 from ECafe.models import Menu
-from connection import get_session
 import repository
 
 app = Blueprint('routes', __name__)
@@ -25,14 +24,14 @@ def menu():
 
     elif request.method == 'POST':
         def get_status(time):
-            if time is not None:
-                status = "Expectation"
+            if time > 0:
+                status = "Готовится"
             else:
-                status = "Done"
+                status = "Готово"
             return status
 
         data = request.get_json()
-        status_time = datetime.now()
+        status_time = data['status_time']
         dish = Menu(title=data['title'],
                     description=data['description'],
                     status=get_status(status_time),
@@ -42,10 +41,21 @@ def menu():
         return {"status": "dish created"}, 201
 
 
-@app.route('/menu/<int:id>', methods=['DELETE'])
-def delete_dish(id):
-    deleted_menu = repository.delete_dish(id)
-    if deleted_menu:
-        return jsonify({"message": f"Personal with id {id} deleted successfully."}), 200
-    return jsonify({"error": f"Personal with id {id} not found."}), 404
+@app.route('/menu/<int:dish_id>', methods=['DELETE'])
+def delete_dish(dish_id):
+    repository.delete_dish(dish_id)
+    return jsonify({"message": f"Menu with id {dish_id} deleted successfully."}), 200
 
+
+@app.route('/price/<int:dish_id>', methods=['PATCH'])
+def price_change(dish_id):
+    data = request.get_json()
+    new_price = data['price']
+    repository.price_change(dish_id, new_price)
+    return jsonify({"message": f"Price of dish with id {dish_id} changed successfully."}), 200
+
+
+@app.route('/status/<int:dish_id>', methods=['PATCH'])
+def status_change(dish_id):
+    repository.status_change(dish_id)
+    return jsonify({"message": f"Status of dish with id {dish_id} changed successfully."}), 200
