65bca352290ea7b99113f7299ada50e1479ffd89
MohammedY555 <157891856+MohammedY555@users.noreply.github.com>
2024-02-01 22:44:28 +0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 # db_url = 'postgresql://username:password@host:port/dbname'
 db_url = 'postgresql://eovqbhzz:OnGBM6Eza9hK6X2u2KGFeCAfmrIMsrM_@kesavan.db.elephantsql.com:5432/eovqbhzz'
-# db_url = 'postgresql://postgres:Mother1505@localhost:5432/ecafe'
 
 # создание машины соединения
 engine = create_engine(db_url, echo=False)
